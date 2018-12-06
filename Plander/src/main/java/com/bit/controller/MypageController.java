40388bcb161de466590dc6462fb22d332919293d
po9357 <po9357@naver.com>
2018-12-06 16:31:33 +0900

    컨트롤러

---
+++ Plander/src/main/java/com/bit/controller/MypageController.java
@@ -0,0 +1,10 @@
+package com.bit.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/Plander/mypage/*")
+public class MypageController {
+
+}
