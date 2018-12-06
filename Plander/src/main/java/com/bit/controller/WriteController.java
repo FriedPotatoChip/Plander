fa54482f3ca27822e4b9c008ea9819f4f04c5a44
po9357 <po9357@naver.com>
2018-12-06 21:23:19 +0900

    write

--- Plander/src/main/java/com/bit/controller/WriteController.java
+++ Plander/src/main/java/com/bit/controller/WriteController.java
@@ -1,20 +1,41 @@
 package com.bit.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.bit.domain.PlannerVO;
 
 @Controller
-@RequestMapping("/Plander/write/*")
+@RequestMapping("/Plander/write")
 public class WriteController {
 
-	@RequestMapping("test")
+	@RequestMapping("/test")
 	public String test() {
-		return "home";
+		return "write/planderWrite";
+	}
+	@RequestMapping("test2")
+	public String test2() {
+		return "write/planderWrite";
 	}
 	
+//	@RequestMapping("/")
+//	public String writeMain() {
+//		return "write/planderWrite";
+//	}
+	@GetMapping("")
+	public String writeMain() {
+		return "write/planderWrite";
+	}
 	
-	
-	
-	
+	@PostMapping("")
+	public String writeMain(@ModelAttribute("PlannerVO")PlannerVO vo) {
+		System.out.println("디테일 옴");
+		return "write/planderWrite_detail";
+	}
 	
 }
---
+++ Plander/src/main/java/com/bit/domain/PlannerVO.java
@@ -0,0 +1,26 @@
+package com.bit.domain;
+
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+public class PlannerVO {
+
+	private int p_idx;
+	private String p_title;
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Date p_start_date;
+	@DateTimeFormat(pattern = "yyyy-MM-dd")	
+	private Date p_end_date;
+	private Date regdate;
+	private int u_idx;
+	private int ct_idx;
+	
+}
