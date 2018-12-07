2c17736c3543e1c82a829dc1a34f566603f7f15d
1027 <1027@hb1027>
2018-12-07 14:51:55 +0900

    View

---
+++ Plander/src/main/java/com/bit/domain/Category_dVO.java
@@ -0,0 +1,15 @@
+package com.bit.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+public class Category_dVO {
+	
+	private int ctd_idx;
+	private int ct_idx;
+	private String ctd_name;
+}
