d506edb825fe5605816cfb0ac22cda3899be29ec
po9357 <po9357@naver.com>
2018-12-26 12:01:13 +0900

    한거없음

--- Plander/src/main/java/com/bit/controller/MainController.java
+++ Plander/src/main/java/com/bit/controller/MainController.java
@@ -1,39 +1,32 @@
 package com.bit.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.bit.service.RecruitService;
 
 @Controller
 @RequestMapping("/TMS")
 public class MainController {
 	
-	@Autowired
-	private RecruitService service;
 	
 	@GetMapping("")
 	public String main() {
 		return "main/main";
 	}
 	
-
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	@RequestMapping("")
-	public String uploadTest() {
-		return "";
+	@GetMapping("/sub")
+	public String sub() {
+		return "main/submain";
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 }
