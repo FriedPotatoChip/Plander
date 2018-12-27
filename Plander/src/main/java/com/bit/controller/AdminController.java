1088bba00dd2398e115b370599024e76c8d67d51
1027 <1027@hb1027>
2018-12-27 17:54:59 +0900

    wonho

--- Plander/src/main/java/com/bit/controller/AdminController.java
+++ Plander/src/main/java/com/bit/controller/AdminController.java
@@ -1,7 +1,6 @@
 package com.bit.controller;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,23 +8,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.bit.domain.BookingCbVO;
-import com.bit.domain.Paging;
+import com.bit.domain.BookingVO;
 import com.bit.domain.UsersVO;
+import com.bit.service.BookService;
 import com.bit.service.adminService;
 import com.bit.utils.PagingVO;
 
 @Controller
 @RequestMapping("/TMS/admin")
 public class AdminController {
-
+	
+	@Autowired
+	private BookService bookservice;
+	
 	@Autowired
 	private adminService service;
 
-	// 페이징 처리
-	Paging p = new Paging();
 
 	@RequestMapping("")
 	public String adminPage(UsersVO vo, Model model, PagingVO page) {
@@ -74,9 +74,12 @@ public class AdminController {
 	
 	@RequestMapping("/Chart")
 	public String ChartPage(Model model) {
-		/*model.addAttribute("dateTime", service.DateTime());
-		List<DateTimeVO> datetime = service.DateTime();
-		System.out.println(datetime.toString());*/
+		
 		return "adminPage/Chart";
+	}
+	
+	@RequestMapping("/Seats")
+	public String SeatsPage(@RequestParam("sct_idx")int sct_idx , Model model, BookingVO bvo) {
+		return "adminPage/Seats";
 	}
 }
--- Plander/src/main/java/com/bit/service/BookService.java
+++ Plander/src/main/java/com/bit/service/BookService.java
@@ -8,12 +8,15 @@ public interface BookService {
 	
 	//전체 좌석 수 확인
 	public int seatCnt(int br_idx, int sct_idx);
+	
 	//예약된 좌석 수(개인실)
 	public int bookoneCnt(BookingVO bvo);
+	
 	//예약된 좌석 조회(개인실)
 	public List<BookingVO> bookone(BookingVO bvo);
 	
 	public int bookroomCnt(BookingVO bvo);
+	
 	public List<BookingVO> bookroom(BookingVO bvo);
 	
 	//선택 정보 조회
